0cca007278ee56dad84c43c567344cd02b18a72d
Paige <phoebeandrew11@gmail.com>
2025-04-09 11:37:14 +0100

    initial commit

--- frontend/anchor-idl/idl.ts
+++ frontend/anchor-idl/idl.ts
@@ -5,7 +5,7 @@
  * IDL can be found at `target/idl/counter.json`.
  */
 export type Counter = {
-  address: "DapSDYWFCrWvAx8FvXcDpvNSNsjTYDoLqVf5tUdG24wA";
+  address: "8ZbyJdWCdkS3mfKJkfJeVsQwH6KmXT2CXMpMM96pmWWk";
   metadata: {
     name: "counter";
     version: "0.1.0";
